35146e88fc4b63844a171b704e3adc459d242d6a
Crussader <parikshit.rao04@gmail.com>
2024-11-16 21:17:18 +0530

    Create routes for creating quizes

--- hpquiz/backend/app.py
+++ hpquiz/backend/app.py
@@ -1,11 +1,35 @@
 from typing import Optional
 from contextlib import asynccontextmanager
 
+from bson import ObjectId
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 
 from .db import Database
+from pydantic import BaseModel
+
+class FormInternal(BaseModel):
+    title: str
+    description: str
+    author: str
+
+    class Config:
+        populate_by_name = True
+     
+class Option(BaseModel):
+    option: str
+    is_correct: bool
+        
+class QuestionInternal(BaseModel):  
+    index: int
+    question: str
+    type: str
+    options: list[Option]
+
+    class Config:
+        populate_by_name = True
+        
+        
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,6 +43,7 @@ async def lifespan(app: FastAPI):
     app.db.client.close()
 
 app = FastAPI(lifespan=lifespan)
+# app = FastAPI()
 db: Database = app.db
 
 # Configure CORS
@@ -54,8 +79,17 @@ async def form(id: Optional[str], name: Optional[str]):
         form["questions"] = questions
         
     return form
-        
-    
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.post("/forms/create")
+async def create_form(form_data: FormInternal):
+    # Make _id None to let MongoDB generate a new ObjectId
+    form_data.id = None
+    form = db.insert_one("forms", form_data.model_dump())
+    return form
+
+@app.post("/forms/create/question")
+async def create_question(form_id: str, question_data: list[QuestionInternal]):
+    data = [{**question.model_dump(), "form_id": form_id} for question in question_data]
+    db.insert_many("questions", data)
+    
+    return {"message": "Questions created successfully."}
